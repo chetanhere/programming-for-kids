4df37fabafa9bf8171026f5c58f6962196c1432c
git_chetan <chetanpandit2011@gmail.com>
2020-06-27 13:15:47 +0530

    first two programs to start with Python Turtle

---
+++ randomturtle.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+for i in range(180):
+    t.circle(100)
+    t.left(2)
+    
---
+++ turtle2.py
@@ -0,0 +1,7 @@
+import turtle
+pia = turtle.Turtle()
+pia.speed(0)
+for i in range(400):
+    pia.forward(i)
+    pia.left(91)
+    
